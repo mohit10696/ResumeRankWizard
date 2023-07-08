0d4dc173939cfbe31a777d6c375d334bd6f289cb
Mohit Rajeshkumar Sojitra <mh546676@dal.ca>
2023-07-08 02:30:05 -0300

    finally file upload fixed

--- file-upload-function/app.js
+++ file-upload-function/app.js
@@ -1,51 +1,26 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
+const parser = require("lambda-multipart-parser");
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const pdf = require('pdf-parse');
+const BUCKET = "resume-classifier";
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-exports.lambdaHandler = async (event, context) => {
-    const content = await s3.getObject({
-        Bucket: 'resume-classifier',
-        Key: 'DockerAssignment.pdf',
-    }).promise();
-    // console.log(content.Body.toString('utf-8'));
-    try {
-        pdf(content.Body).then(function(data) {
- 
-            // number of pages
-            console.log("Pages",countInstances(data.text, 'docker'));                
-        });
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: event.message,
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+exports.lambdaHandler = async (event) => {
+  const result = await parser.parse(event);
+  console.log(result.files);
+  const file = result.files[0];
 
-    return response
+  const fileContent = Buffer.from(file.content, 'base64'); // Convert the file content to Buffer
+
+  await s3.putObject({
+    Bucket: BUCKET,
+    Key: file.filename,
+    Body: fileContent,
+    ContentType: file.contentType, // Set the appropriate Content-Type
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      files: file.filename,
+    }),
+  };
 };
-
-
-function countInstances(string, word) {
-    return string.split(word).length - 1;
- }
